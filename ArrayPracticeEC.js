0bfe5cf78f4a3dbd4d195ec2943706d785015650
croswog <112280219+croswog@users.noreply.github.com>
2022-11-28 13:36:03 -0600

    Add ArrayPracticeEC files

---
+++ ArrayPracticeEC.js
@@ -0,0 +1,37 @@
+//Task 1
+
+var arrayNames = new Array("John", "Robert", "Jimmy", "Bonzo");
+
+var arrayInstruments = [];
+arrayInstruments[0] = "bass";
+arrayInstruments[1] = "vocals";
+arrayInstruments[2] = "guitar";
+arrayInstruments[3] = "drums";
+
+var stringMessage = "";
+
+var intCount;
+
+for (intCount = 0; intCount < arrayNames.length; intCount++)
+{
+	stringMessage = stringMessage + arrayNames[intCount] + " plays " + arrayInstruments[intCount] + "\n";
+}
+
+alert(stringMessage);
+
+//task 2
+
+var stringTimesTable = "";
+
+for (var x=1; x<=12; x++)
+{
+	for (var y=1; y<=12; y++)
+	{
+		stringTimesTable = stringTimesTable + " " + x*y;
+	}
+	stringTimesTable = stringTimesTable + "\n";
+}
+
+alert(stringTimesTable);
+
+//task 3
